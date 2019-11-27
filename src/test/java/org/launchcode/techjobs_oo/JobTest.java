cd56b67defc7f5ff8cda7a9aab674acbfcee24cc
laalaunch <lastembo@gmail.com>
2019-11-27 07:27:20 -0600

    added @Before setup

--- src/test/java/org/launchcode/techjobs_oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs_oo/JobTest.java
@@ -3,18 +3,29 @@ package org.launchcode.techjobs_oo;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Objects;
+
 import static org.junit.Assert.*;
 
 public class JobTest {
 
-    @Test
-    public void testSettingJobId() {
-        Job id1 = new Job();
-        Job id2 = new Job();
-        int job_id1 = id1.hashCode();
-        int job_id2 = id2.hashCode();
+    private int test_id1;
+    private int test_id2;
 
-        assertEquals(job_id1, job_id2, 1.0);
+    @Before
+    public void setUp() throws Exception {
+        test_id1 = new Job().hashCode();
+        test_id2 = new Job().hashCode();
     }
 
+    @Test
+    public void testSettingJobId() {
+        int result = test_id2 - test_id1;
+        assertEquals("Unique Job Ids should differ by 1", 1, result);
+    }
+
+//    public void testJobConstructorSetsAllFields() {
+//        Job job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+//        System.out.println(Job());
+//    }
 }
